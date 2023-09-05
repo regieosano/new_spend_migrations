ade3666cf90d57f110a858440be9b2a421e5bcaf
regieosano <osanoreginald@gmail.com>
2023-09-05 23:13:28 +0800

    Updated transactions.py codes

--- new_spend_tables/transactions/transactions.py
+++ new_spend_tables/transactions/transactions.py
@@ -11,4 +11,16 @@ source_csv_file = "transactions/transaction_test_data_dump.csv"
 session_pool = sessionmaker(engine)
 
 with open(source_csv_file, newline='') as csvfile:
-    pass
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        transaction = Transactions(
+            id=row['id'],
+            source=row['source_id'],
+            external_id=row['external_id'],
+            invoice_id=row[''],
+            is_reconciled=False,
+        )
+
+        with Session(engine) as session:
+            session.add(transaction)
+            session.commit()
