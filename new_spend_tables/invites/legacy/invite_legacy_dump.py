da7f4b91899d669179e2334f32d6100c2b24b568
jed <johncd@codev.com>
2023-09-29 20:44:15 +0800

    org_id for invites

--- new_spend_tables/invites/legacy/invite_legacy_dump.py
+++ new_spend_tables/invites/legacy/invite_legacy_dump.py
@@ -1,6 +1,8 @@
 import csv
 from zipfile import ZipFile
 from invite_class_legacy import Invite
+from new_spend_tables.team_player.legacy.team_player_legacy import TeamPlayer
+from new_spend_tables.groups.legacy.team_class_legacy import Team
 from sqlalchemy.orm import Session, sessionmaker
 from _database.engine_db_legacy import engine as spend_dev_engine
 from cuid import cuid
@@ -19,11 +21,18 @@ with open('new_spend_tables/invites/legacy/dump/invite_data_dump.csv', 'w+', new
         'status',
         'created_at',
         'updated_at',
+        'org_id',
     ])
     id_lookup = {}
     with Session(spend_dev_engine) as session:
         records = session.query(Invite).all()
         for col in records:
+            org_id = ''
+            team_player = session.query(TeamPlayer).filter_by(invite_id=col.id).first()
+            if team_player:
+                team = session.query(Team).filter_by(id=team_player.team_id).first()
+                if team.org_id:
+                    org_id = team.org_id
             id = 'spdinv_%s' % cuid()
             id_lookup[col.id] = id
             outcsv.writerow([
@@ -35,7 +44,7 @@ with open('new_spend_tables/invites/legacy/dump/invite_data_dump.csv', 'w+', new
                 col.status,
                 col.created_at,
                 col.updated_at,
-
+                org_id,
             ])
         with open('new_spend_tables/invites/idlookup/invites_id_lookup.json', 'w+') as id_lookup_file:
             json.dump(id_lookup, id_lookup_file)
